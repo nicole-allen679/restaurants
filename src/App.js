ab0d8c220b6e453defdc994ae637d9fe67df6bfb
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-05 20:16:52 -0400

    created react app for restaurants near boca code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [allRestaurants, setAllRestaurants] = useState()
+  const [text, setText] = useState('Please enter your name')
+  useEffect(() => {
+    fetch('https://bocacode-intranet-api.web.app/restaurants')
+      .then((response) => response.json())
+      .then((promise) => setAllRestaurants(promise))
+      .catch((err) => console.log(err))
+  }, [])
+
+  function handleSubmit(event){
+    console.log(allRestaurants)
+    event.preventDefault()
+
+    const newRestaurant = {
+      name: 'ice cream shop',
+      address: '123main st',
+      rating: 6,
+    }
+
+    fetch('https://bocacode-intranet-api.web.app/restaurants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newRestaurant)
+    })
+    .then(response => response.json)
+    .then(status => console.log(status))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          <input type="text" onChange={(e) => setText(e.target.value)} value={text} />
+        </label>
+        <button onClick={event => handleSubmit(event)}>Go Here</button>
+      </form>
+
+      {!allRestaurants ? (
+        <h2>Loading...</h2>
+      ) : (
+        allRestaurants.map((restaurant) => {
+          return (
+            <div key={restaurant.id}>
+              <h2>{restaurant.name}</h2>
+              <span>{restaurant.address}</span>
+              <span>{restaurant.cuisine}</span>
+              <span>{restaurant.rating}</span>
+              <img
+                src={restaurant.photoUrl}
+                alt={`${restaurant.name}`}
+                style={{ maxWidth: '300px', borderRadius: '50%' }}
+              />
+            </div>
+          )
+        })
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
